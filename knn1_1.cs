3e4cf4ad24b45746b8e4761250f6742a288f02a2
anvbrest <an_ni@tut.by>
2016-10-09 17:08:50 +0300

    fist

---
+++ knn1_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint count = 2;            
+            String [] array = new String[count];
+
+            for (int i = 0; i < count; i++)
+            {   
+                    array[i] = Console.ReadLine();  
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = array[i].Insert(0, "X: ");
+                int indComa = array[i].IndexOf(",");
+                array[i] = array[i].Insert(indComa + 1, " Y: ");
+                Console.WriteLine(array[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
